34d6a05c00b64ace2f7cb4bc8f6c4db593d51db5
TheCoolDoggo <thecooldoggobusiness@gmail.com>
2025-09-23 17:52:51 -0400

    player movement, level design, code + more

--- player.gd
+++ player.gd
@@ -1,25 +1,42 @@
 extends CharacterBody2D
 
+const SPEED = 500.0
 
-const SPEED = 900.0
-const JUMP_VELOCITY = -900.0
+var jump_velocity = -700.0
+var was_in_air = false
+var player_score=0
 
+@onready var sprite = $Sprite2D
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+@onready var score_label = get_node("/root/Node/CanvasLayer/Label")
 
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
-	if not is_on_floor():
-		velocity += get_gravity() * delta
+	velocity.y += gravity * delta
 
-	# Handle jump.
-	if Input.is_action_just_pressed("jump") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+	var just_landed = is_on_floor() and was_in_air
+	if just_landed:
+		randomize_sprite_color()
+		was_in_air = false
+	
+	if not is_on_floor() and not is_on_ceiling():
+		was_in_air = true
+		
+	if Input.is_action_just_pressed("flip_gravity"):
+		gravity *= -1
+		jump_velocity *= -1
+		sprite.scale.y *= -1
+		
+	if Input.is_action_just_pressed("jump") and (is_on_floor() or is_on_ceiling()):
+		velocity.y = jump_velocity
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction := Input.get_axis("left", "right")
 	if direction:
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
-
 	move_and_slide()
+
+func randomize_sprite_color():
+	sprite.modulate = Color.from_hsv(randf(), 1.0, 1.0)
+	player_score=player_score+1
+	score_label.text = "Bounces: " + str(player_score)
